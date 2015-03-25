0f6d756d22dae4ec7b413b11985e4b2d63a335cd
Stefan Mintert <oe34sm@linkwerk.com>
2015-03-25 15:31:19 +0100

    Re-arranging of source code. Added some function descriptions.

--- exportUtils.js
+++ exportUtils.js
@@ -28,6 +28,9 @@
 	var DROPATTRIBUTES = [];
 
 
+	/* ********************************************
+	 * 	XML helper functions
+	 */
 	var createLineElement = function(lineAttributes, lineContentString, lmkr) {
 		var lineStartTag = '<line';
 
@@ -57,6 +60,16 @@
 		return '</attribute><!-- /' + key + ' -->';
 	}
 
+
+
+
+
+	/* ********************************************
+	 * EPL specific functions
+	 *
+	 */
+
+
 	/*
 	 * Unfortunately Changeset.stringIterator does not reveal it's current index.
 	 * This function calculates the index from the original string length
@@ -66,6 +79,8 @@
 	var getIteratorIndex = function(stringIterator, stringLength) {
 		return (stringLength - stringIterator.remaining());
 	}
+
+
 
 
 	/*
@@ -101,32 +116,10 @@
 
 
 
-	// returns null if no URLs, or [[startIndex1, url1], [startIndex2, url2], ...]
-	var findURLs = function (text) {
-		// copied from ACE
-		var _REGEX_WORDCHAR = /[\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF\u0100-\u1FFF\u3040-\u9FFF\uF900-\uFDFF\uFE70-\uFEFE\uFF10-\uFF19\uFF21-\uFF3A\uFF41-\uFF5A\uFF66-\uFFDC]/;
-		var _REGEX_SPACE = /\s/;
-		var _REGEX_URLCHAR = new RegExp('(' + /[-:@a-zA-Z0-9_.,~%+\/\\?=&#;()$]/.source + '|' + _REGEX_WORDCHAR.source + ')');
-		var _REGEX_URL = new RegExp(/(?:(?:https?|s?ftp|ftps|file|smb|afp|nfs|(x-)?man|gopher|txmt):\/\/|mailto:)/.source + _REGEX_URLCHAR.source + '*(?![:.,;])' + _REGEX_URLCHAR.source, 'g');
-
-
-		_REGEX_URL.lastIndex = 0;
-		var urls = null;
-		var execResult;
-		while ((execResult = _REGEX_URL.exec(text))) {
-			urls = (urls || []);
-			var startIndex = execResult.index;
-			var url = execResult[0];
-			urls.push([startIndex, url]);
-		}
-
-		return urls;
-	}
-
-
 	/*
+	 * analyzeLine
+	 * Lists discovery
 	 *
-	 * TODO outsource this function
 	 */
 	var analyzeLine = function (text, aline, apool, reqOptions) {
 
@@ -393,6 +386,38 @@
 		}
 		return orderedEndTagsString;
 	}
+
+
+
+
+
+	/* ********************************************
+	 * Regexp analysis
+	 *
+	 */
+
+	// returns null if no URLs, or [[startIndex1, url1], [startIndex2, url2], ...]
+	var findURLs = function (text) {
+		// copied from ACE
+		var _REGEX_WORDCHAR = /[\u0030-\u0039\u0041-\u005A\u0061-\u007A\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF\u0100-\u1FFF\u3040-\u9FFF\uF900-\uFDFF\uFE70-\uFEFE\uFF10-\uFF19\uFF21-\uFF3A\uFF41-\uFF5A\uFF66-\uFFDC]/;
+		var _REGEX_SPACE = /\s/;
+		var _REGEX_URLCHAR = new RegExp('(' + /[-:@a-zA-Z0-9_.,~%+\/\\?=&#;()$]/.source + '|' + _REGEX_WORDCHAR.source + ')');
+		var _REGEX_URL = new RegExp(/(?:(?:https?|s?ftp|ftps|file|smb|afp|nfs|(x-)?man|gopher|txmt):\/\/|mailto:)/.source + _REGEX_URLCHAR.source + '*(?![:.,;])' + _REGEX_URLCHAR.source, 'g');
+
+
+		_REGEX_URL.lastIndex = 0;
+		var urls = null;
+		var execResult;
+		while ((execResult = _REGEX_URL.exec(text))) {
+			urls = (urls || []);
+			var startIndex = execResult.index;
+			var url = execResult[0];
+			urls.push([startIndex, url]);
+		}
+
+		return urls;
+	}
+
 
 
 
