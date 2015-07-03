c321aad4ce5310bac6d19ab97afa94c472a46503
Thomas Muehlichen <ext_github_tm@linkwerk.com>
2015-07-03 14:51:59 +0200

    20150702 Refactoring Part I

--- commentsXml.js
+++ commentsXml.js
@@ -1,169 +1,168 @@
-(function() {
-	var ERR = require("ep_etherpad-lite/node_modules/async-stacktrace");
+var ERR = require("ep_etherpad-lite/node_modules/async-stacktrace");
 
-    var commentsPlugin = false;
-    var jsxml = false;
-
-
-    try {
-    	commentsPlugin = require("../ep_comments_page/commentManager.js");
-    } catch (e) {
-    	console.log("Can't load comments plug-in.");
-    	console.log(JSON.stringify(e));
-    }
-
-    try {
-    	jsxml = require("jsxml");
-    } catch (e) {
-    	console.log("Can't load jsxml. Implication: I can't use comments plug-in.");
-    	console.log(JSON.stringify(e));
-    	commentsPlugin = false;
-    }
+var commentsPlugin = false;
+var jsxml = false;
 
 
-	var commentsXml = (function() {
+try {
+    commentsPlugin = require("../ep_comments_page/commentManager.js");
+} catch (e) {
+    console.log("Can't load comments plug-in.");
+    console.log(JSON.stringify(e));
+}
 
-		// allComments = all comments saved for a pad (including those for older pad versions)
-		var allComments = null;
+try {
+    jsxml = require("jsxml");
+} catch (e) {
+    console.log("Can't load jsxml. Implication: I can't use comments plug-in.");
+    console.log(JSON.stringify(e));
+    commentsPlugin = false;
+}
 
-		// currentPadComments = only those comments, that are required for the current pad content
-		var currentPadComments = [];
 
-		var init = function(padId) {
+    var commentsXml = (function() {
 
-			currentPadComments = [];
+            // allComments = all comments saved for a pad (including those for older pad versions)
+            var allComments = null;
 
-            if (commentsPlugin) {
+            // currentPadComments = only those comments, that are required for the current pad content
+            var currentPadComments = [];
 
-            	try {
-	            	commentsPlugin.getComments(padId, function(err, padComments) {
-	            		ERR(err);
+            var init = function(padId) {
 
-	            		commentsPlugin.getCommentReplies(padId, function(err, commentReplies) {
-	                		ERR(err);
+                    currentPadComments = [];
 
-	                		allComments = {
-	                				comments: padComments.comments,
-	                				replies: commentReplies.replies
-	                		};
+        if (commentsPlugin) {
 
-	                	});
-	            	});
-            	} catch (e) {
-            		console.log("[ERROR] Could not retrieve comments." + e);
-            	}
+            try {
+                    commentsPlugin.getComments(padId, function(err, padComments) {
+                            ERR(err);
 
+                            commentsPlugin.getCommentReplies(padId, function(err, commentReplies) {
+                                    ERR(err);
+
+                                    allComments = {
+                                                    comments: padComments.comments,
+                                                    replies: commentReplies.replies
+                                    };
+
+                            });
+                    });
+            } catch (e) {
+                    console.log("[ERROR] Could not retrieve comments." + e);
             }
-		};
 
-		var selectComment = function(commentId) {
-			currentPadComments.push(commentId.toString());
-		};
+        }
+            };
 
-		var getCommentsXml = function() {
-			var xmlString = "";
+            var selectComment = function(commentId) {
+                    currentPadComments.push(commentId.toString());
+            };
 
-			if (allComments && currentPadComments.length > 0) {
-				xmlString = "<comments>";
+            var getCommentsXml = function() {
+                    var xmlString = "";
 
-
-				for (var i = 0; i < currentPadComments.length; i++) {
-					xmlString += commentToXml(currentPadComments[i]);
-				}
-				xmlString += "</comments>\n";
-			}
-
-			return xmlString;
-		};
-
-		var commentToXml = function(commentId) {
-			if (allComments.comments && allComments.comments[commentId]) {
-				var newCommentElement = [
-				                          "comment",
-				                          {
-				                        	  "id": commentId.toString(),
-				                        	  "timestamp": allComments.comments[commentId].timestamp.toString(),
-				                        	  "isoDateTime": (new Date(allComments.comments[commentId].timestamp)).toISOString()
-				                          },
-				                         ];
+                    if (allComments && currentPadComments.length > 0) {
+                            xmlString = "<comments>";
 
 
-				var newAuthorElement = [
-				                        	"author",
-				                        	{
-				                        		"id": allComments.comments[commentId].author.toString()
-				                        	},
-				                        	allComments.comments[commentId].name.toString()
-				                        	];
+                            for (var i = 0; i < currentPadComments.length; i++) {
+                                    xmlString += commentToXml(currentPadComments[i]);
+                            }
+                            xmlString += "</comments>\n";
+                    }
 
-				newCommentElement.push(newAuthorElement);
+                    return xmlString;
+            };
 
-				var newTextElement = [
-				                      	"text",
-				                      	{},
-				                      	allComments.comments[commentId].text.toString()
-				                      ];
-
-
-				newCommentElement.push(newTextElement);
-
-
-				var replies = ["replies",{}];
-
-				for (var replyId in allComments.replies) {
-					if (allComments.replies.hasOwnProperty(replyId) && allComments.replies[replyId].commentId == commentId) {
-
-						var reply = [
-						             "comment",
-						             {
-						            	 "id": replyId.toString(),
-						            	 "timestamp": allComments.replies[replyId].timestamp.toString(),
-			                        	  "isoDateTime": (new Date(allComments.replies[replyId].timestamp)).toISOString()
-						             },
-									[
-										"author",
-										{
-											"id": allComments.replies[replyId].author.toString()
-										},
-										allComments.replies[replyId].name.toString()
-									],
-									[
-									 	"text",
-									 	{},
-									 	allComments.replies[replyId].text.toString()
-									 ]
-					              ];
-
-						replies.push(reply);
-					}
-				}
-
-				if (replies.length > 2 ) { // only add replies element if there are reply children
-					newCommentElement.push(replies);
-				}
+            var commentToXml = function(commentId) {
+                    if (allComments.comments && allComments.comments[commentId]) {
+                            var newCommentElement = [
+                                                      "comment",
+                                                      {
+                                                              "id": commentId.toString(),
+                                                              "timestamp": allComments.comments[commentId].timestamp.toString(),
+                                                              "isoDateTime": (new Date(allComments.comments[commentId].timestamp)).toISOString()
+                                                      },
+                                                     ];
 
 
-				return "\n" + jsxml.toXml(newCommentElement);
+                            var newAuthorElement = [
+                                                            "author",
+                                                            {
+                                                                    "id": allComments.comments[commentId].author.toString()
+                                                            },
+                                                            allComments.comments[commentId].name.toString()
+                                                            ];
 
-			}
+                            newCommentElement.push(newAuthorElement);
 
-		};
+                            var newTextElement = [
+                                                    "text",
+                                                    {},
+                                                    allComments.comments[commentId].text.toString()
+                                                  ];
+
+
+                            newCommentElement.push(newTextElement);
+
+
+                            var replies = ["replies",{}];
+
+                            for (var replyId in allComments.replies) {
+                                    if (allComments.replies.hasOwnProperty(replyId) && allComments.replies[replyId].commentId == commentId) {
+
+                                            var reply = [
+                                                         "comment",
+                                                         {
+                                                             "id": replyId.toString(),
+                                                             "timestamp": allComments.replies[replyId].timestamp.toString(),
+                                                      "isoDateTime": (new Date(allComments.replies[replyId].timestamp)).toISOString()
+                                                         },
+                                                                    [
+                                                                            "author",
+                                                                            {
+                                                                                    "id": allComments.replies[replyId].author.toString()
+                                                                            },
+                                                                            allComments.replies[replyId].name.toString()
+                                                                    ],
+                                                                    [
+                                                                            "text",
+                                                                            {},
+                                                                            allComments.replies[replyId].text.toString()
+                                                                     ]
+                                                  ];
+
+                                            replies.push(reply);
+                                    }
+                            }
+
+                            if (replies.length > 2 ) { // only add replies element if there are reply children
+                                    newCommentElement.push(replies);
+                            }
+
+
+                            return "\n" + jsxml.toXml(newCommentElement);
+
+                    }
+
+            };
 
 
 
-		return {
-			init: init,
-			selectComment: selectComment,
-			getCommentsXml: getCommentsXml
-		};
-	})();
+            return {
+                    init: init,
+                    selectComment: selectComment,
+                    getCommentsXml: getCommentsXml
+            };
+    })();
 
-    /*
-     * Define exports
-     *
-     */
-    exports.init 			= commentsXml.init;
-    exports.selectComment 	= commentsXml.selectComment;
-    exports.getCommentsXml 	= commentsXml.getCommentsXml;
-})();
+/*
+ * Define exports
+ *
+ */
+exports.init 			= commentsXml.init;
+exports.selectComment 	= commentsXml.selectComment;
+exports.getCommentsXml 	= commentsXml.getCommentsXml;
+
 
--- hooks.js
+++ hooks.js
@@ -4,48 +4,51 @@ var exportXml = require('./ExportXml');
 var prettyData = false;
 
 try {
-	prettyData = require('pretty-data').pd;
+    prettyData = require('pretty-data').pd;
 } catch (e) {
-	console.log("Can't load pretty-data.");
-	console.log(JSON.stringify(e));
+    console.log("Can't load pretty-data.");
+    console.log(JSON.stringify(e));
 }
 
 
-
 exports.expressCreateServer = function (hook_name, args, cb) {
-	args.app.get('/p/:pad/:rev?/export/xml', function(req, res, next) {
-		var padID = req.params.pad;
-		var options = {
-				revision: (req.params.rev ? req.params.rev : null),
-				lists: (req.query.lists ? req.query.lists.toString() === "true" : false),
-				lineattribs: (req.query.lineattribs ? req.query.lineattribs.toString() === "true" : false),
-				regex: (req.query.regex ? req.query.regex.toString() === "true" : false),
-				pretty: (req.query.pretty ? req.query.pretty.toString() === "true" : false),
-		};
-		
-		
-		exportXml.getPadXmlDocument(padID, options, function(err, result) {
-			res.contentType('plain/xml');
-			if (prettyData && options.pretty) {
-				res.send(prettyData.xml(result));
-			} else {
-				res.send(result);
-			}
-		});
-	});
+    var _evalToBoolean = function(stringValue) {
+        return typeof stringValue !== 'undefined' && stringValue === 'true';
+    };
+    
+    args.app.get('/p/:pad/:rev?/export/xml', function(req, res, next) {
+        var padID = req.params.pad;
+        var options = {
+            revision:   (req.params.rev ? req.params.rev : null),
+            lists:       _evalToBoolean(req.query.lists),
+            lineattribs: _evalToBoolean(req.query.lineattribs),
+            regex:       _evalToBoolean(req.query.regex),
+            pretty:      _evalToBoolean(req.query.pretty)
+        };
+
+
+        exportXml.getPadXmlDocument(padID, options, function(err, result) {
+            res.contentType('plain/xml');
+            if (prettyData && options.pretty) {
+                res.send(prettyData.xml(result));
+            } else {
+                res.send(result);
+            }
+        });
+    });
 };
 
 exports.eejsBlock_exportColumn = function(hook_name, args, cb) {
-	args.content = args.content + eejs.require("ep_xmlexport/templates/exportcolumn.html", {}, module);
-	return cb();
+    args.content = args.content + eejs.require("ep_xmlexport/templates/exportcolumn.html", {}, module);
+    return cb();
 };
 
 exports.eejsBlock_scripts = function (hook_name, args, cb) {
-	args.content = args.content + eejs.require("ep_xmlexport/templates/scripts.html", {}, module);
-	return cb();
+    args.content = args.content + eejs.require("ep_xmlexport/templates/scripts.html", {}, module);
+    return cb();
 };
 
 exports.eejsBlock_styles = function (hook_name, args, cb) {
-	args.content = args.content + eejs.require("ep_xmlexport/templates/styles.html", {}, module);
-	return cb();
+    args.content = args.content + eejs.require("ep_xmlexport/templates/styles.html", {}, module);
+    return cb();
 };
