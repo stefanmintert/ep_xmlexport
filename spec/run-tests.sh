9e67552f8b74cbd5252bfe1271196cf437f2dda9
Thomas Muehlichen <ext_github_tm@linkwerk.com>
2015-07-03 13:13:11 +0200

    created integration tests

---
+++ spec/run-tests.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+ETHERPAD_HOST="http://localhost:49001"
+
+mkdir -p outputXml
+
+# -r prevents backslash escapes from being interpreted.
+# || [[ -n $line ]] prevents the last line from being ignored if it doesn't end with a \n (since read returns a non-zero exit code when it encounters EOF).
+while read -r line || [[ -n $line ]]; do
+    # skip lines beginning with '#'
+    [[ "$line" =~ ^#.*$ ]] && continue
+    # skip empty lines
+    [[ "$line" =~ ^$ ]] && continue
+    
+    echo "run testcase: $line"
+    TESTCASE="test-$line"
+    curl --silent $ETHERPAD_HOST/p/$TESTCASE/export/xml > outputXml/$TESTCASE.xml
+    curl --silent $ETHERPAD_HOST/p/$TESTCASE/export/xml?lineattribs=true > outputXml/$TESTCASE-lineattribs.xml
+    curl --silent $ETHERPAD_HOST/p/$TESTCASE/export/xml?lists=true > outputXml/$TESTCASE-lists.xml
+    curl --silent $ETHERPAD_HOST/p/$TESTCASE/export/xml?regex=true > outputXml/$TESTCASE-regex.xml
+    curl --silent $ETHERPAD_HOST/p/$TESTCASE/export/xml?pretty=true > outputXml/$TESTCASE-pretty.xml
+done < "testcases.list" 
+
+diff -q referenceXml/ outputXml/ && echo "[OK] Tests run successfully!"
